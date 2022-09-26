521599da3ea1947c78b0af3d0d3eb571f843230f
Glitch (glitch-hello-website) <none>
2022-09-26 13:39:55 +0000

    🌒🕍 Checkpoint ./index.html:270926/143 ./script.js:270926/1430 ./app.js:270926/514095 ./pokemon.js:270926/61

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+console.log("Hello world!!!") 
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+let x = 0
+console.log(x)
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+let cs396 = {
+  students: 317,
+  assignments: ["hello web", "animated gif", "drawing"] 
+  
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+for (let i = 0; i < cs396.assignments; i++) {
+  console.log("assignment ", i)
+  // printHello(i)
+}
+
+
+function printHello(x) {
+  console.log("hello!", x)
+}
+
+console.log(cs396)
+// console.log(pokemon)
