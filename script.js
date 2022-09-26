b71e51bc7aa367f99cda2b07d1d20cf4d5e71d0a
Glitch (glitch-hello-website) <none>
2022-09-26 13:51:54 +0000

    🎬⛱ Checkpoint ./script.js:270926/886 ./index.html:270926/258

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 console.log("Hello world!!!") 
 
-let x = 0
-console.log(x)
+localStorage.setItem("name", "Dr Kate")
 
-let cs396 = {
-  students: 317,
-  assignments: ["hello web", "animated gif", "drawing"] 
+function changeName() {
+  let titleEl = document.getElementById("pagetitle")
+  console.log(titleEl)
+  console.dir(titleEl)
+    
+  
+  // titleEl.innerText += "Dr Kate" + Math.random()
+  
+//   let pEl = document.createElement("p")
+//   pEl.innerText = "hi"
+//   titleEl.appendChild(pEl)
   
 }
 
-for (let i = 0; i < cs396.assignments; i++) {
-  console.log("assignment ", i)
-  // printHello(i)
-}
+// let x = 0
+// console.log(x)
+
+// let cs396 = {
+//   students: 317,
+//   assignments: ["hello web", "animated gif", "drawing"] 
+  
+// }
+
+// for (let i = 0; i < cs396.assignments.length; i++) {
+//   console.log("assignment ", i, cs396.assignments[i])
+//   // printHello(i)
+// }
 
 
-function printHello(x) {
-  console.log("hello!", x)
-}
+// function printHello(x) {
+//   console.log("hello!", x)
+// }
 
-console.log(cs396)
-// console.log(pokemon)
+// console.log(cs396)
+// // console.log(pokemon)
